0f945cddd4ef5497a20ede6dd497f2e52530a33e
Anthony Williams <anthony.williams.cs@gmail.com>
2025-04-19 00:32:38 -0700

    Add JSDocs

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,14 @@
+/**
+ * @description Energy used for new players
+ */
 export const DEFAULT_ENERGY = 10
+/**
+ * @description HP used for new players
+ */
 export const DEFAULT_HP = 100
+
+/**
+ * @description Time (in milliseconds) to wait before restoring energy to each
+ * player
+ */
+export const ENERGY_TIMER_MS = 60 * 1000 // 1 minute
