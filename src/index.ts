75112993a57c9548140b93bbf470b6976c681843
Anthony Williams <anthony.williams.cs@gmail.com>
2025-04-20 22:14:58 -0700

    Use GameService for index

--- src/index.ts
+++ src/index.ts
@@ -1,154 +1,65 @@
 import express, { type Request, type Response } from 'express'
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import type { Server } from 'node:http'
-import { resolve } from 'node:path'
-import {
-	ENERGY_TIMER_MS,
-	PERMS_READ_WRITE_EXEC,
-	SAVE_TIMER_MS,
-} from './constants'
 import { Weapon } from './enums'
-import { BossService } from './services/BossService'
-import { PlayerService } from './services/PlayerService'
-import type { ActiveBoss, AttackResult, GameState } from './types'
+import { GameService } from './services/GameService'
+import type { ActiveBoss, AttackResult } from './types'
 import { generateDocs, log } from './utils'
 
 // grab port from env or default to 3000
 const port = process.env.PORT ?? 3000
 
-// persistence
-const saveDir = resolve(__dirname, '..', 'save-data')
-const savePath = resolve(saveDir, 'currentState.json')
+const gameService = GameService()
 
-// create services
-const bossService = BossService()
-const playerService = PlayerService()
-
-const weaponDamage: Record<Weapon, number> = {
-	[Weapon.Shield]: 0,
-	[Weapon.Fist]: 2,
-	[Weapon.Wand]: 5,
-	[Weapon.Staff]: 6,
-	[Weapon.Dagger]: 7,
-	[Weapon.Bow]: 8,
-	[Weapon.Spear]: 9,
-	[Weapon.Sword]: 10,
-	[Weapon.Crossbow]: 10,
-	[Weapon.Mace]: 11,
-	[Weapon.Axe]: 12,
-	[Weapon.Flail]: 13,
-	[Weapon.Hammer]: 14,
-	[Weapon.Scythe]: 15,
-	[Weapon.Whip]: 4,
-	[Weapon.Gun]: 20,
-	[Weapon.Grenade]: 25,
-	[Weapon.Bomb]: 30,
-	[Weapon.Rocket]: 35,
-	[Weapon.Laser]: 40000,
-}
-
+/**
+ * This function is called when the server shuts down
+ */
 const onServerShutdown = (server: Server, sig: NodeJS.Signals) => {
 	log(`Received signal: ${sig}`)
 	log('HTTP server is shutting down...')
 
-	if (timerEnergy) {
-		clearInterval(timerEnergy as NodeJS.Timeout)
-	}
-	if (timerSave) {
-		clearInterval(timerSave as NodeJS.Timeout)
-	}
-
-	saveToDisk()
-
-	server.close(() => {
-		log('HTTP server closed')
-	})
-}
-
-const onServerStart = () => {
-	timerEnergy = setInterval(() => {
-		log(`Energy timer tick`)
-
-		playerService
-			.restoreEnergy()
-			.then(() => {
-				lastEnergyRestore = new Date()
-			})
-			.catch((err) => log(err, true))
-	}, ENERGY_TIMER_MS)
-
-	timerSave = setInterval(() => {
-		log(`Save timer tick`)
-
-		saveToDisk()
-
-		lastSave = new Date()
-	}, SAVE_TIMER_MS)
-
-	log(`Server is running on port ${port}`)
-}
-
-const saveToDisk = () => {
-	log('Saving game state to disk...')
-
-	// create save dir if missing
-	if (!existsSync(saveDir)) {
-		mkdirSync(saveDir, {
-			mode: PERMS_READ_WRITE_EXEC,
+	gameService
+		.shutdown()
+		.then(() => {
+			log('Game service shutdown complete')
 		})
-	}
+		.finally(() => {
+			server.close(() => {
+				log('HTTP server closed')
+			})
+		})
+}
 
-	const saveData: GameState = {
-		bosses: bossService.statuses,
-		lastEnergyRestore: lastEnergyRestore?.getTime() ?? Date.now(),
-		lastSave: lastSave?.getTime() ?? Date.now(),
-		players: playerService.statuses,
-		version: 1,
-	}
+/**
+ * This function is called when the server starts
+ */
+const onServerStart = () => {
+	log(`Server is running on port ${port}`)
 
-	// create save file
-	writeFileSync(savePath, JSON.stringify(saveData, null, 2), {
-		encoding: 'utf-8',
-		mode: PERMS_READ_WRITE_EXEC,
+	gameService.start().then(() => {
+		log('Game service started')
 	})
 }
 
-let lastEnergyRestore: Date = new Date()
-let lastSave: Date = new Date()
-let parsedGameState: GameState | undefined = undefined
-let timerEnergy: NodeJS.Timeout | undefined = undefined
-let timerSave: NodeJS.Timeout | undefined = undefined
-
-// load save file (if exists)
-if (existsSync(savePath)) {
-	const data = readFileSync(savePath, {
-		encoding: 'utf-8',
-		flag: 'r',
-	})
-
-	parsedGameState = JSON.parse(data) as GameState
-}
-
-// first load data from saved game state or disk
-Promise.all([
-	bossService.load(parsedGameState),
-	playerService.load(parsedGameState),
-])
+// load game service
+gameService
+	.load()
 	.then(() => {
 		// create server (express app)
 		const app = express()
+
+		// !! ----------
+		// !! Only setup middleware and routes if game service loaded successfully
+		// !! ----------
 
 		// middleware
 		app.use(express.json())
 		app.use(express.urlencoded({ extended: true }))
 
-		// !! Only setup routes if data loaded successfully
+		// routes
 		app.get('/', (req: Request, res: Response) => {
 			const docHtml = generateDocs({
-				bossService,
+				gameService,
 				pageTitle: 'Boss Fight API',
-				playerService,
-				weaponDamage,
 			})
 
 			res.status(200).contentType('text/html').send(docHtml)
@@ -168,8 +79,8 @@ Promise.all([
 
 			const normalizedBossQuery = String(name).toLowerCase().trim()
 
-			const boss = bossService.statuses[normalizedBossQuery]
-			const p = playerService.statuses[player]
+			const boss = gameService.bossService.statuses[normalizedBossQuery]
+			const p = gameService.playerService.statuses[player]
 
 			if (!boss) {
 				res.status(400).json({ error: 'Boss not found' })
@@ -186,8 +97,12 @@ Promise.all([
 				return
 			}
 
-			const attackResult: AttackResult = await bossService
-				.attack(normalizedBossQuery, p, weaponDamage[p.selectedWeapon])
+			const attackResult: AttackResult = await gameService.bossService
+				.attack(
+					normalizedBossQuery,
+					p,
+					gameService.weaponDamage[p.selectedWeapon],
+				)
 				.catch((err) => err)
 
 			if (attackResult?.error) {
@@ -196,27 +111,28 @@ Promise.all([
 			}
 
 			const enegeryBeforeAttack =
-				playerService.statuses[p.name].currentEnergy
+				gameService.playerService.statuses[p.name].currentEnergy
 			const enegeryAfterAttack = enegeryBeforeAttack - 1
-			const hpBeforeAttack = playerService.statuses[p.name].currentHp
+			const hpBeforeAttack =
+				gameService.playerService.statuses[p.name].currentHp
 			const hpAfterAttack = hpBeforeAttack - attackResult.bossDamage
 
 			// if attack results in negative energy, set to 0; otherwise subtract energy
-			playerService.statuses[p.name].currentEnergy =
+			gameService.playerService.statuses[p.name].currentEnergy =
 				enegeryAfterAttack < 0 ? 0 : enegeryAfterAttack
 			// if attack results in negative hp, set to 0; otherwise subtract damage
-			playerService.statuses[p.name].currentHp =
+			gameService.playerService.statuses[p.name].currentHp =
 				hpAfterAttack < 0 ? 0 : hpAfterAttack
 
 			res.status(201).json({
-				boss: bossService.statuses[normalizedBossQuery],
-				player: playerService.statuses[p.name],
+				boss: gameService.bossService.statuses[normalizedBossQuery],
+				player: gameService.playerService.statuses[p.name],
 			})
 		})
 
 		// boss routes
 		app.get('/boss', (req: Request, res: Response) => {
-			res.status(200).json(bossService.statuses)
+			res.status(200).json(gameService.bossService.statuses)
 		})
 
 		app.get('/boss/:idOrName', (req: Request, res: Response) => {
@@ -224,7 +140,9 @@ Promise.all([
 				.toLowerCase()
 				.trim()
 
-			const [, boss] = Object.entries(bossService.statuses).find(
+			const [, boss] = Object.entries(
+				gameService.bossService.statuses,
+			).find(
 				([bossName, activeBoss]) =>
 					bossName.includes(normalizedBossQuery) ||
 					activeBoss.id.toString().includes(normalizedBossQuery),
@@ -241,9 +159,10 @@ Promise.all([
 		// player routes
 		app.get('/player', (req: Request, res: Response) => {
 			res.status(200).json(
-				playerService.players.map((p) => ({
+				gameService.playerService.players.map((p) => ({
 					...p,
-					selectedWeaponDamage: weaponDamage[p.selectedWeapon],
+					selectedWeaponDamage:
+						gameService.weaponDamage[p.selectedWeapon],
 					selectedWeaponName: Weapon[p.selectedWeapon],
 				})),
 			)
@@ -254,7 +173,7 @@ Promise.all([
 				.toLowerCase()
 				.trim()
 
-			const player = playerService.players.find(
+			const player = gameService.playerService.players.find(
 				(p) =>
 					p.id.toString().includes(normalizedPlayerQuery) ||
 					p.name.toLowerCase().includes(normalizedPlayerQuery),
@@ -265,11 +184,12 @@ Promise.all([
 				return
 			}
 
-			const status = playerService.statuses[player.name]
+			const status = gameService.playerService.statuses[player.name]
 
 			res.status(200).json({
 				...status,
-				selectedWeaponDamage: weaponDamage[player.selectedWeapon],
+				selectedWeaponDamage:
+					gameService.weaponDamage[player.selectedWeapon],
 				selectedWeaponName: Weapon[player.selectedWeapon],
 			})
 		})
@@ -280,7 +200,7 @@ Promise.all([
 			const normalizedPlayerQuery = String(name).toLowerCase().trim()
 			const isValidWeapon = Object.values(Weapon).includes(selectedWeapon)
 			const playerExists =
-				playerService.players.find(
+				gameService.playerService.players.find(
 					(p) => p.name.toLowerCase() === normalizedPlayerQuery,
 				) !== undefined
 
@@ -289,28 +209,25 @@ Promise.all([
 				return
 			}
 
-			playerService.add(name, selectedWeapon)
+			gameService.playerService.add(name, selectedWeapon)
 
-			res.status(201).json(playerService.statuses[name])
+			res.status(201).json(gameService.playerService.statuses[name])
 		})
 
 		// timer routes
-		app.get('/timer', (req: Request, res: Response) => {
-			res.status(200).json({
-				energyTimer: ENERGY_TIMER_MS,
-				lastEnergyRestore: lastEnergyRestore.toISOString(),
-				lastEnergyRestoreTimestamp: lastEnergyRestore.getTime(),
-				lastSave: lastSave.toISOString(),
-				lastSaveTimestamp: lastSave.getTime(),
-				saveTimer: SAVE_TIMER_MS,
-			})
+		app.get('/timer', async (req: Request, res: Response) => {
+			const status = await gameService.getStatus()
+
+			res.status(200).json(status)
 		})
 
 		// weapon routes
 		app.get('/weapon', (req: Request, res: Response) => {
 			const weaponEntries = Object.entries(Weapon).map(
 				([enumNumber, enumValueName]) => ({
-					damage: weaponDamage[enumNumber as unknown as Weapon],
+					damage: gameService.weaponDamage[
+						enumNumber as unknown as Weapon
+					],
 					name: enumValueName,
 				}),
 			)
@@ -338,7 +255,9 @@ Promise.all([
 			}
 
 			res.status(200).json({
-				damage: weaponDamage[weaponEntries[0] as unknown as Weapon],
+				damage: gameService.weaponDamage[
+					weaponEntries[0] as unknown as Weapon
+				],
 				name: weaponEntries[1],
 			})
 		})
@@ -361,7 +280,7 @@ Promise.all([
 		// })
 	})
 	.catch((err) => {
-		log('Error loading data from disk', true)
+		log('Error loading game service', true)
 		log(err, true)
 		log('Exiting...', true)
 
